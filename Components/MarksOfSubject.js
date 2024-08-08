c4238dd45d240e60039ecf666242390f9a3845d8
BazilSuhail <bazil1854@gmail.com>
2024-08-08 18:54:49 +0500

    Updated Attedance and Marks ...

--- Components/MarksOfSubject.js
+++ Components/MarksOfSubject.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { View, Text, FlatList } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 
+import { Ionicons } from '@expo/vector-icons'; // For icons
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
 const MarksOfSubject = () => {
     const route = useRoute();
     const { selectedCourseMarks, courseName, errorMessage } = route.params || {};
@@ -17,35 +20,73 @@ const MarksOfSubject = () => {
 
     return (
         <View className="flex-1 p-4 bg-custom-blue">
-           {selectedCourseMarks ? (
+            {selectedCourseMarks ? (
                 <View>
+                    <Text className="text-xl font-bold text-white my-4">Obtained Marks</Text>
+                    <View>
+                        <View className="flex-row bg-blue-500 py-2 rounded-t-lg">
+                            <Text className="flex-1 w-[170px] text-center text-white font-bold">Assesment Name</Text>
+                            <Text className="flex-1 text-center text-white font-bold">Obtained Marks</Text>
+                            <Text className="flex-1 text-center text-white font-bold">Total Marks</Text>
+                            <Text className="flex-1 text-center text-white font-bold">Weighted Marks</Text>
+                        </View>
+                        <FlatList
+                            data={selectedCourseMarks.criteriaDefined}
+                            keyExtractor={(item, index) => index.toString()}
+                            renderItem={({ item }) => (
+                                <View className="flex-row justify-between py-2 bg-blue-950 border-b border-gray-500">
+                                    <Text className="text-white font-extrabold pl-[4px] text-center flex-1">{item.assessment}</Text>
+                                    <Text className="text-white text-center flex-1">{selectedCourseMarks.studentMarks[item.assessment]}</Text>
+                                    <Text className="text-white text-center flex-1">{item.totalMarks}</Text>
+                                    <Text className="text-white text-center flex-1">
+                                        {((selectedCourseMarks.studentMarks[item.assessment] / item.totalMarks) * item.weightage).toFixed(2)}
+                                    </Text>
+                                </View>
+                            )}
+                        />
+                    </View>
+
+                    <View className="flex-row mt-[35px] px-[5px] justify-between space-x-3 mb-4">
+                        <View className="bg-blue-900 flex-row items-center p-3 rounded-lg w-[49%] ">
+                            <View className="flex-row items-center">
+                                <Ionicons name="school-outline" size={38} color="white" />
+                                <View className="h-[40px] mx-[10px] w-[3px] bg-white "></View>
+                            </View>
+                            <View className="flex ml-[5px] justify-center items-center">
+                                <Text className="text-gray-200 font-bold text-[14px] mb-[2px]">Total Marks</Text>
+                                <Text className="text-white text-[22px] font-bold ">{calculateTotalWeightedMarks()}</Text>
+                            </View>
+                        </View>
+
+                        <View className="bg-blue-900 flex-row items-center p-3 rounded-lg w-[49%] ">
+                            <View className="flex-row items-center">
+                                <MaterialIcons name="grade" size={38} color="white" />
+                                <View className="h-[40px] mx-[10px] w-[3px] bg-white "></View>
+                            </View>
+                            <View className="flex ml-[22px] justify-center items-center">
+                                <Text className="text-gray-200 font-bold text-[14px] mb-[2px]">Grade</Text>
+                                <Text className="text-white text-[22px] font-bold ">{selectedCourseMarks.grade}</Text>
+                            </View>
+                        </View>
+                    </View>
+
                     <Text className="text-xl font-bold text-white underline my-4">Grading Criteria</Text>
+                    <View className="flex-row bg-blue-500 py-2 rounded-t-lg">
+                            <Text className="flex-1 w-[170px] text-center text-white font-bold">Assesment Name</Text>
+                            <Text className="flex-1 text-center text-white font-bold">Decided Weightage</Text>
+                         </View>
                     <FlatList
                         data={selectedCourseMarks.criteriaDefined}
                         keyExtractor={(item, index) => index.toString()}
                         renderItem={({ item, index }) => (
-                            <View className="flex-row justify-between bg-blue-900 rounded-md py-[10px] border-b border-gray-500">
+                            <View className="flex-row justify-between bg-blue-900 py-[10px] border-b border-gray-500">
                                 <Text className="text-white font-medium text-center flex-1">{item.assessment}</Text>
                                 <Text className="text-white font-extrabold text-center flex-1">{item.weightage}%</Text>
                             </View>
                         )}
                     />
 
-                    <Text className="text-xl font-bold text-white my-4">Obtained Marks</Text>
-                    <FlatList
-                        data={selectedCourseMarks.criteriaDefined}
-                        keyExtractor={(item, index) => index.toString()}
-                        renderItem={({ item }) => (
-                            <View className="flex-row justify-between py-2 border-b border-gray-700">
-                                <Text className="text-white text-center flex-1">{item.assessment}</Text>
-                                <Text className="text-white text-center flex-1">{selectedCourseMarks.studentMarks[item.assessment]}</Text>
-                                <Text className="text-white text-center flex-1">{item.totalMarks}</Text>
-                                <Text className="text-white text-center flex-1">
-                                    {((selectedCourseMarks.studentMarks[item.assessment] / item.totalMarks) * item.weightage).toFixed(2)}
-                                </Text>
-                            </View>
-                        )}
-                    />
+
 
                     <View className="mt-5">
                         <View className="flex-row justify-between py-3 border-b border-gray-700">
--- Components/StudentAttendanceDetails.js
+++ Components/StudentAttendanceDetails.js
@@ -69,16 +69,17 @@ const StudentAttendanceDetails = () => {
       {loading ? (
         <ActivityIndicator size="large" color="#0000ff" />
       ) : error ? (
-        <Text className="text-red-500 text-center my-4">
-          Error: {error}
+        <Text className="text-red-500 p-3 border-2 border-red-500 rounded-lg text-center my-4">
+          {error}
         </Text>
       ) : (
         <View>
           <View className="mb-4">
 
+           {/*
             <View className="w-[100%] mb-[15px] flex flex-row justify-center space-x-2">
               <View className="bg-blue-900 p-3 rounded-2xl w-[48%]">
-                <Text className="text-sm bg-blue-950 rounded-lg text-blue-500 text-center py-[5px] mb-[7px] font-bold">Total Days</Text>
+                <Text className="text-sm bg-blue-950 rounded-lg text-blue-200 text-center py-[5px] mb-[7px] font-bold">Total Days</Text>
                 <Text className="text-white text-center font-semibold text-3xl">{totalDays}</Text>
               </View>
               <View className="bg-blue-950 p-3 rounded-2xl w-[48%]">
@@ -94,25 +95,48 @@ const StudentAttendanceDetails = () => {
                 <Text className="text-white text-center font-semibold text-3xl">{totalDays - daysPresent}</Text>
               </View>
               <View className="bg-blue-900 p-3 rounded-2xl w-[48%]">
-                <Text className="text-sm bg-blue-800 rounded-lg text-blue-100 text-center py-[5px] mb-[7px] font-bold">Overall Attendance %</Text>
+                <Text className="text-sm bg-blue-600 rounded-lg text-blue-100 text-center py-[5px] mb-[7px] font-bold">Overall Attendance %</Text>
                 <Text className="text-white text-center font-semibold text-3xl">{attendancePercentage}%</Text>
               </View>
             </View>
+           */}
+
           </View>
+
+          <View className="mb-[15px] flex bg-blue-900 p-3 rounded-2xl w-full">
+
+            <View className="pt-3 flex-row justify-between rounded-2xl w-full">
+              <Text className="bg-blue-950 rounded-lg text-md text-white py-[8px] w-[120px] text-center mb-[7px] font-bold">Attendance %</Text>
+              <View className="flex-row space-x-2">
+                <Text className="text-sm bg-blue-950 rounded-lg text-blue-200 w-[100px] text-center mt-[2px] pt-[5px] h-[30px] py-auto  font-bold">Days Present</Text>
+                <Text className="text-sm bg-blue-950 rounded-lg text-blue-200 w-[100px] text-center mt-[2px] pt-[5px] h-[30px] py-auto  font-bold">Days Absent</Text>
+              </View>
+            </View>
+
+            <View className="flex-row justify-between rounded-2xl w-full">
+              <Text className="text-2xl text-gray-300 text-center w-[120px] font-bold">{attendancePercentage} %</Text>
+              <View className="flex-row space-x-2">
+                <Text className="text-3xl text-center text-gray-300  w-[100px] mb-[7px] font-bold">{daysPresent}</Text>
+                <Text className="text-3xl text-center text-gray-300  w-[100px] mb-[7px] font-bold">{totalDays - daysPresent}</Text>
+              </View>
+            </View>
+
+          </View>
+
           {attendanceRecords.length > 0 ? (
 
-                <View>
-                      <View className="flex-row bg-blue-500 py-2 rounded-t-lg">
-          <Text className="flex-1 w-[170px] text-center text-white font-bold">Date</Text>
-          <Text className="flex-1 text-center w-[90px] text-white font-bold">Status</Text> 
-        </View>
+            <View>
+              <View className="flex-row bg-blue-500 py-2 rounded-t-lg">
+                <Text className="flex-1 w-[170px] text-center text-white font-bold">Date</Text>
+                <Text className="flex-1 text-center w-[90px] text-white font-bold">Status</Text>
+              </View>
 
               <FlatList
                 data={attendanceRecords}
                 keyExtractor={(item) => item.date}
                 renderItem={({ item }) => (
                   <View className="flex-row py-2 rounded-b-lg bg-blue-950 text-white">
-                    <Text  className="text-center w-[170px] font-medium my-auto text-white px-2">{item.date}</Text>
+                    <Text className="text-center w-[170px] font-medium my-auto text-white px-2">{item.date}</Text>
                     <Text
                       className={`text-lg p-1 w-[100px] mx-auto rounded-lg font-semibold text-center ${item.records[currentUser.uid] ? 'bg-green-800 text-white' : 'bg-red-800 text-white'
                         }`}
@@ -120,14 +144,12 @@ const StudentAttendanceDetails = () => {
                       {item.records[currentUser.uid] ? 'Present' : 'Absent'}
                     </Text>
                   </View>
-                    )}
-                    
-                    
+                )}
               />
             </View>
 
           ) : (
-            <Text className="text-red-500 text-center my-4">No attendance records found.</Text>
+            <Text className="text-red-300 p-3 border-2 border-red-200 rounded-lg text-center my-4">No attendance records found.</Text>
           )}
         </View>
       )}
